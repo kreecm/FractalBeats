e0d7ba017432eef90b7d89b370da430fe399456b
Michael Andrews <michael.112358.andrews@gmail.com>
2017-05-19 14:37:12 -0700

    Add files via upload
    fractal2d.h: no changes except ordering to enable commenting, commenting out get_grid, and adding a const version of operator()
    fractal2d.cpp: no changes except order of function inputs (because I'm a weirdo and preferred that order)
    mandelbrot.h: no changes except ordering to enable commenting
    mandelbrot.cpp: no changes except commenting
    grid2d.h: made "at" private, fixed a bug ( m_data(new T[width * height]()) in order to initialize values correctly )

--- grid2d.h
+++ grid2d.h
@@ -5,91 +5,90 @@
 #include <memory>
 #include <Eigen/Eigen>
 
-/**
- * A generic container for storing objects of type T in a 2-dimensional grid.
- */
 template<typename T>
 class grid2d {
- public:
-  using coord = Eigen::Vector2i;
-  using value_type = T;
+    public:
+        using value_type = T;
+        using coord = Eigen::Vector2i;                  // for pairs of integers
 
-  grid2d(int width, int height) :
-      m_size(width, height),
-      m_data(new T[width * height])
-  {}
+    
+    private:
+        std::unique_ptr<T[]> m_data;                    // we store a grid's worth of values of type T
+        coord m_size;                                   // the size of the grid is given by a pair of integers
+    
+    public:
+        grid2d(int width, int height) :                 // we can initialize a grid of certain width and height
+            m_data(new T[width * height]()),
+            m_size(width, height)
+        {}
 
-  int width() const { return m_size[0]; }
-  int height() const { return m_size[1]; }
+        int width() const { return m_size[0]; }
+        int height() const { return m_size[1]; }
+        int size() const { return width() * height(); }
 
-  int size() const { return width() * height(); }
+    
+    private:
+        int linear_index(const coord& node) const {     // since we store a grid's worth of data in an array
+            assert(0 <= node[0] && node[0] < width());
+            assert(0 <= node[1] && node[1] < height());
+        
+            return node[0] + node[1] * width();         // we use row-major indexing
+        }
+    
+    private:
+        T& at(const coord& node) {                      // we can look at an element in the grid
+            int index = linear_index(node);
+            return m_data[index];
+        }
+    
+        const T& at(const coord& node) const {          // even if the grid is constant
+            int index = linear_index(node);
+            return m_data[index];
+        }
 
-  T& operator[] (const coord& node) { return at(node); }
-  const T& operator[] (const coord& node) const { return at(node); }
+    public:
+        T& operator[] (const coord& node) { return at(node); }                              // public notation is
+        const T& operator[] (const coord& node) const { return at(node); }                  // [] if we use a coord
 
-  T& operator() (int i, int j) { return at(coord(i, j)); }
-  const T& operator() (int i, int j) const { return at(coord(i, j)); }
+        T& operator() (const int i, const int j) { return at(coord(i, j)); }                // public notation is ()
+        const T& operator() (const int i, const int j) const { return at(coord(i, j)); }    // if we use a pair of ints
 
-  T& at(const coord& node) {
-    int index = linear_index(node);
-    return m_data[index];
-  }
-
-  const T& at(const coord& node) const {
-    int index = linear_index(node);
-    return m_data[index];
-  }
-
-  /**
-   * Constructs a new grid that is smaller in each dimension by one.
-   *
-   * If we consider the calling object to store its data at the nodes of a
-   * 2-dimensional grid, the returned object is suitable for storing data at the
-   * cells of the grid.
-   */
-  template<typename U>
-  std::unique_ptr<grid2d<U>> create_cell_grid() {
-    auto* cell_grid = new grid2d<U>(width() - 1, height() - 1);
-    return std::unique_ptr<grid2d<U>>(cell_grid);
-  }
-
-  /**
-   * Constructs a new grid that is smaller in horizontal dimension by one.
-   *
-   * If we consider the calling object to store its data at the nodes of a
-   * 2-dimensional grid, the returned object is suitable for storing data on the
-   * horizontal edges of the grid.
-   */
-  template<typename U>
-  std::unique_ptr<grid2d<U>> create_horiz_edge_grid() {
-    auto* cell_grid = new grid2d<U>(width() - 1, height());
-    return std::unique_ptr<grid2d<U>>(cell_grid);
-  }
-
-  /**
-   * Constructs a new grid that is smaller in vertical dimension by one.
-   *
-   * If we consider the calling object to store its data at the nodes of a
-   * 2-dimensional grid, the returned object is suitable for storing data on the
-   * vertical edges of the grid.
-   */
-  template<typename U>
-  std::unique_ptr<grid2d<U>> create_vert_edge_grid() {
-    auto* cell_grid = new grid2d<U>(width(), height() - 1);
-    return std::unique_ptr<grid2d<U>>(cell_grid);
-  }
-
- private:
-  coord m_size;
-  std::unique_ptr<T[]> m_data;
-
-  int linear_index(const coord& node) const {
-    assert(0 <= node[0] && node[0] < width());
-    assert(0 <= node[1] && node[1] < height());
-
-    // row-major indexing
-    return node[0] + node[1] * width();
-  }
+    /**
+     * Constructs a new grid that is smaller in each dimension by one.
+     *
+     * If we consider the calling object to store its data at the nodes of a
+     * 2-dimensional grid, the returned object is suitable for storing data at
+     * the cells of the grid.
+     */
+        template<typename U>
+        std::unique_ptr<grid2d<U>> create_cell_grid() {
+            auto* cell_grid = new grid2d<U>(width() - 1, height() - 1);
+            return std::unique_ptr<grid2d<U>>(cell_grid);
+        }
+    /**
+     * Constructs a new grid that is smaller in horizontal dimension by one.
+     *
+     * If we consider the calling object to store its data at the nodes of a
+     * 2-dimensional grid, the returned object is suitable for storing data on
+     * the horizontal edges of the grid.
+     */
+        template<typename U>
+        std::unique_ptr<grid2d<U>> create_horiz_edge_grid() {
+            auto* cell_grid = new grid2d<U>(width() - 1, height());
+            return std::unique_ptr<grid2d<U>>(cell_grid);
+        }
+    /**
+     * Constructs a new grid that is smaller in vertical dimension by one.
+     *
+     * If we consider the calling object to store its data at the nodes of a
+     * 2-dimensional grid, the returned object is suitable for storing data on
+     * the vertical edges of the grid.
+     */
+        template<typename U>
+        std::unique_ptr<grid2d<U>> create_vert_edge_grid() {
+            auto* cell_grid = new grid2d<U>(width(), height() - 1);
+            return std::unique_ptr<grid2d<U>>(cell_grid);
+        }
 };
 
 #endif // FRACTAL_BEATS_GRID2D_H
--- mandelbrot.h
+++ mandelbrot.h
@@ -3,26 +3,25 @@
 
 #include "fractal2d.h"
 
-/**
- * Implements the Mandelbrot set as a fractal2d.
- *
- * The default domain for the Mandelbrot set is [-2.5, 0.5] x [-1, 1]. The
- * resolution specifies the number of grid points in the y-direction. The number
- * of grid points in the x-direction is 1.5 * resolution (since the domain has
- * 3:2 aspect ratio.)
- */
 class mandelbrot : public fractal2d {
- public:
-  mandelbrot(int resolution, int max_iter);
+    
+    // Default domain is [-2.5, 0.5] x [-1, 1].
+    // The resolution specifies the number of grid points in the y-direction.
+    // The number of grid points in the x-direction is 1.5 * resolution
+    // (since the domain has 3:2 aspect ratio).
+    
+    private:
+        const int m_resolution;
+        const int m_max_iter;               // this is the threshold after which we assert escape will not occur
+    
+    public:
+        mandelbrot(int resolution, int max_iter);
 
- protected:
-  void generate() final;
-
- private:
-  const int m_resolution;
-  const int m_max_iter;
-
-  bool test_point(const point& p);
+    private:
+        bool test_point(const point& p);    // this tests an arbitrary complex number to see whether it is in M
+    
+    protected:
+        void generate() final;              // this generates the Mandelbrot set
 };
 
 #endif // FRACTAL_BEATS_MANDELBROT_H
